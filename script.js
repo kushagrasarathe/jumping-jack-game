b44a2de0efd3fc63e8a58376f6e4008a612217c4
Kushagra <kushagraaa.01@gmail.com>
2022-02-21 15:32:25 +0530

    game completed

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 // variables
 
 const hero = document.querySelector(".hero");
-const hero_boy =  document.querySelector("hero-boy");
+const heroboy =  document.querySelector("hero-boy");
 const villain = document.querySelector(".villain");
+
+// score variables
+const score = document.querySelector(".score");
+const resetNum = document.querySelector(".reset");
 
 // functions
 
 const jump = () => {
     if (hero.classList != "animate") {
-        hero.classList.add(".animate");
+        hero.classList.add("animate");
         villain.style.animation = "move 1s infinite linear";       
     }
-}
+    
+    // setTimeout function calls a finction only once
+    setTimeout(() => {
+        hero.classList.remove("animate");
+    }, 300);
 
+};
 
 document.addEventListener("keydown", (e) => {
     if (e.code == "Space"){
+        let initialScore = 0;
+        score.innerHTML++  ;
         jump();
-    }
+    } 
 });
+
+// functionality when boy clashes with wolf
+
+let isAlive = setInterval(() => {
+    
+    let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+    //console.log(heroTop);
+    let villianLeft = parseInt(window.getComputedStyle(villain).getPropertyValue("left"));
+    //console.log(villianLeft);
+    
+    if (villianLeft < 40 && villianLeft > 20 && heroTop >= 130) {
+        villain.style.animation = "none";
+        alert("Game Over Boiii!!!", score.innerHTML = 0);
+    }
+}, 10);
+
